65d63b1a787830c2baad4e0fe2a27506b01e603b
FlashbangCat <eundor03@naver.com>
2025-07-18 15:42:52 +0900

    leh PostCreate comple

--- src/main/java/com/missionx/questloggers/domain/post/service/PostService.java
+++ src/main/java/com/missionx/questloggers/domain/post/service/PostService.java
@@ -1,5 +1,8 @@
 package com.missionx.questloggers.domain.post.service;
 
+import com.missionx.questloggers.domain.post.dto.CreatePostRequestDto;
+import com.missionx.questloggers.domain.post.dto.CreatePostResponseDto;
+import com.missionx.questloggers.domain.post.entity.Post;
 import com.missionx.questloggers.domain.post.entity.Post;
 import com.missionx.questloggers.domain.post.exception.NotFoundException;
 import com.missionx.questloggers.domain.post.repository.PostRepository;
@@ -19,5 +22,18 @@ public class PostService {
                 .orElseThrow(() -> new NotFoundException(HttpStatus.NOT_FOUND,
                         "게시글을 찾을 수 없습니다. 다시 확인해주세요"));
     }
+
+    public CreatePostResponseDto createPostService(CreatePostRequestDto createPostRequestDto) {
+        //임시 유저 아이디
+        Long id = 1L;
+
+        String dtoTitle = createPostRequestDto.getTitle();
+        String dtoContent = createPostRequestDto.getContent();
+        Post newPost = new Post(dtoTitle, dtoContent);
+        Post savedPost = postRepository.save(newPost);
+        CreatePostResponseDto createPostResponseDto = new CreatePostResponseDto(savedPost.getId(), savedPost.getTitle(), savedPost.getContent());
+        return createPostResponseDto;
+    }
+
 
 }
