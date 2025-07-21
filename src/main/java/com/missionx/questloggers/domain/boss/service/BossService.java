dafa60ae3ccc964a355ee6ee7df32cb919c06a46
FlashbangCat <eundor03@naver.com>
2025-07-21 14:26:18 +0900

    leh 보스 생성 API 완

--- src/main/java/com/missionx/questloggers/domain/boss/service/BossService.java
+++ src/main/java/com/missionx/questloggers/domain/boss/service/BossService.java
@@ -1,12 +1,24 @@
 package com.missionx.questloggers.domain.boss.service;
 
+import com.missionx.questloggers.domain.boss.dto.CreateBossRequestDto;
+import com.missionx.questloggers.domain.boss.dto.CreateBossResponseDto;
+import com.missionx.questloggers.domain.boss.entity.Boss;
 import com.missionx.questloggers.domain.boss.repository.BossRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class BossService {
 
     public final BossRepository bossRepository;
+
+    @Transactional
+    public CreateBossResponseDto createBossService(CreateBossRequestDto createBossRequestDto) {
+        Boss newBoss = new Boss(createBossRequestDto);
+        Boss savedBoss = bossRepository.save(newBoss);
+        return new CreateBossResponseDto(savedBoss.getId(), savedBoss.getBossName(), savedBoss.getBossImage());
+    }
+
 }
