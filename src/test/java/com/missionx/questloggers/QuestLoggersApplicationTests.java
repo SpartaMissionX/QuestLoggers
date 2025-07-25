2479066837e3ff18c55ba354a7718b054ef03a7b
FlashbangCat <eundor03@naver.com>
2025-07-25 16:00:18 +0900

    leh 배포용 프로파일 변경

--- src/test/java/com/missionx/questloggers/QuestLoggersApplicationTests.java
+++ src/test/java/com/missionx/questloggers/QuestLoggersApplicationTests.java
@@ -1,9 +1,7 @@
 package com.missionx.questloggers;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class QuestLoggersApplicationTests {
 
     @Test
