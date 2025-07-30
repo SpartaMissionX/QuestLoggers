025259b6d312dbbcea4d2cad271b096ff1c9e3a3
mong9ssi <mong9ssi@naver.com>
2025-07-30 16:36:03 +0900

    PartyApplicant domain 분리

---
+++ src/main/java/com/missionx/questloggers/domain/partyapplicant/service/PartyApplicantSupportService.java
@@ -0,0 +1,4 @@
+package com.missionx.questloggers.domain.partyapplicant.service;
+
+public class PartyApplicantService {
+}
---
+++ src/main/java/com/missionx/questloggers/domain/partymember/repository/PartyMemberRepository.java
@@ -0,0 +1,4 @@
+package com.missionx.questloggers.domain.partymember;
+
+public interface PartyMemberRepository {
+}
