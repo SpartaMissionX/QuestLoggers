8af7070c090f3fafdd5b6716f789cae63939fe10
mong9ssi <mong9ssi@naver.com>
2025-08-07 12:11:11 +0900

    refactor : produces 수정

--- src/main/java/com/missionx/questloggers/domain/notification/controller/SseEmitterController.java
+++ src/main/java/com/missionx/questloggers/domain/notification/controller/SseEmitterController.java
@@ -3,6 +3,7 @@ package com.missionx.questloggers.domain.notification.controller;
 import com.missionx.questloggers.domain.notification.service.SseEmiterService;
 import com.missionx.questloggers.global.config.security.LoginUser;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,7 +17,7 @@ import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 public class SseEmitterController {
     private final SseEmiterService sseEmiterService;
 
-    @GetMapping(value = "/posts/{postId}/applicant/subscribe", produces = "text/event-stream")
+    @GetMapping(value = "/posts/{postId}/applicant/subscribe", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     public SseEmitter subscribe(
             @PathVariable Long postId,
             @AuthenticationPrincipal LoginUser loginUser
